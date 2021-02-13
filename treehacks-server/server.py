a2f660dfca4a1d416a21bfa7919a69a11a31308c
Bimesh De Silva <b2desilv@edu.uwaterloo.ca>
2021-02-13 18:09:01 -0500

    websockets

--- treehacks-server/server.py
+++ treehacks-server/server.py
@@ -1,7 +1,24 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
+# from flask import Flask
+# from flask import request
+import asyncio
+import websockets
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+async def hello(websocket, path):
+    # name = await websocket.recv()
+    # print(f"< {name}")
+
+    greeting = f"Hello bob!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
